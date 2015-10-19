a37bfc2ac29c7f68f4f0718550dfe79e83faed17
Samuel Hastings <shastings517@gmail.com>
2015-10-19 10:48:53 -0700

    add DRY restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   root 'pins#index'
-  get '/pins', to: 'pins#index'
-  post '/pins', to: 'pins#create'
-  get '/pins/new', to: 'pins#new'
-  get '/pins/:id', to: 'pins#show', as: :pin
-  put '/pins/:id', to: 'pins#update'
-  patch '/pins/:id', to: 'pins#update'
-  get '/pins/:id/edit', to: 'pins#edit'
-  delete '/pins/:id', to: 'pins#destroy'
+  resources :pins #CREATES ALL RESTFUL ROUTES FOR RESOURCE
+  # get '/pins', to: 'pins#index'
+  # post '/pins', to: 'pins#create'
+  # get '/pins/new', to: 'pins#new'
+  # get '/pins/:id', to: 'pins#show', as: :pin
+  # put '/pins/:id', to: 'pins#update'
+  # patch '/pins/:id', to: 'pins#update'
+  # get '/pins/:id/edit', to: 'pins#edit'
+  # delete '/pins/:id', to: 'pins#destroy'
 end
 
 # http VERB 'route', to: 'controller#action'
